241ab373be39af4cbdc4af126aec6f9eb7be1882
Renars Miljons <reku.miljons@gmail.com>
2022-09-05 01:18:03 +0300

    mobile support

--- src/components/BackButton/BackButton.js
+++ src/components/BackButton/BackButton.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import { useNavigate } from 'react-router-dom';
 import arrowBack from "../../assets/icons/arrow-back.svg";
-import "./backButton.css"
+import "./BackButton.css"
 
 export function BackButton() {
     const navigate = useNavigate();
